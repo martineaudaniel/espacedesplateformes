aecad06d158ddc03eb8b7a0794ab2860f4f491db
Daniel Martineau <danielmartineau18@gmail.com>
2024-05-24 17:51:44 -0400

    Ajout d'une hauteur custom pour le séparateur en mode 'mobile'

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      height: {
+        0.25: '0.0625rem', // Adds h-0.25 class for 1px height
+      },
+    },
   },
   plugins: [],
   darkMode: ['class', '.darkmode'],
