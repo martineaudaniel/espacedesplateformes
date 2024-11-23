590aa3f0c1acb7d4da1c6eca9d3c6901ad0a0f1a
Daniel Martineau <danielmartineau18@gmail.com>
2024-11-23 19:58:49 +0100

    Arrangé (pour de vrai cette fois) le bug avec le nombre de colonnes dans la gallerie qui n'était pas respecté

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,4 +10,9 @@ module.exports = {
   },
   plugins: [],
   darkMode: ['class', '.darkmode'],
+  // Tailwind CSS uses a "purge" process that scans your files for class names and includes only those it finds in the final CSS bundle.
+  // Since we're dynamically generating class names for the number of columns in the PhotoSwipe gallery (e.g., 'columns-2'),
+  // Tailwind doesn't detect them during this scan and thus doesn't include the corresponding CSS styles.
+  // The solution is to force Tailwind to include all the columns classes in the final generated CSS.
+  safelist: ['columns-1', 'columns-2', 'columns-3', 'columns-4', 'columns-5', 'columns-6'],
 }
